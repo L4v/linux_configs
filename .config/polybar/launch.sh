08a2a863740a3db65b721e2661975ecf973e52b7
L4v <ivosevic.jovan@uns.ac.rs>
2021-01-27 07:58:00 +0100

    :art: Reformated polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # NOTE(Jovan): Get i3 workspace names
-sh generate-ws-icon-list.sh
+# sh generate-ws-icon-list.sh
 # Launch Polybar, using default config location ~/.confi/polybar/config
 polybar mainbar -r &
 echo "Polybar launched..."
